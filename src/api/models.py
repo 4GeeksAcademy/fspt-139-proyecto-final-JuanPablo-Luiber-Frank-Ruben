81a0e4dbfe8c15dd6fa4485dee34d760d6512d26
kneloo <crisjedy1475@gmail.com>
2026-08-26 08:46:40 +0000

    rama develop creada

--- src/api/models.py
+++ src/api/models.py
@@ -5,10 +5,9 @@ from sqlalchemy.orm import Mapped, mapped_column
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
+    steam_id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
 
     def serialize(self):
