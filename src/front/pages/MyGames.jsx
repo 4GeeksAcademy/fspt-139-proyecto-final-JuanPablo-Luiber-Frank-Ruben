2598d80a7e2b4cf9406e80fa20d7605f5bfca8a3
rubensasa <rebensasa@gmail.com>
2026-09-02 19:30:10 +0000

    añadiendo partes

---
+++ src/front/pages/MyGames.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useMemo, useState } from "react";
+import { Navigate } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer";
+import useFavorites from "../hooks/useFavorites";
+import { api } from "../services/api";
+import { Gamecard } from "../components/GameCard";
+
+const FILTERS = [
+    {key: "all", Label: "Todos"},
+    {key: "favorites", label: "Favoritos"},
+    {key: "recent", label: "Mas jugados"},
+];
+
+//Endpoints para sacar los juegos del usuario
+
+export const MyGames = () => {
+    const { store } = useGlobalReducer();
+    const { favorites, toggleFavorite }= useFavorites(store.token);
+    const [ userGames, setUser]
+}
