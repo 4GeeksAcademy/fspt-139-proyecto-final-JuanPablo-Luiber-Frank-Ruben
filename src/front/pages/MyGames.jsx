d3d6894929ecb2df5105d2f301e75d566822e4a2
rubensasa <rebensasa@gmail.com>
2026-09-04 19:36:24 +0000

    reviritiendo cambios

--- src/front/pages/MyGames.jsx
+++ src/front/pages/MyGames.jsx
@@ -1,15 +1,21 @@
 import { useEffect, useMemo, useState } from "react";
-import { Navigate } from "react-router-dom";
 import useGlobalReducer from "../hooks/useGlobalReducer";
-import useFavorites from "../hooks/useFavorites";
-import { api } from "../services/api";
-import { Gamecard } from "../components/GameCard";
 
 const FILTERS = [
     {key: "all", Label: "Todos"},
     {key: "favorites", label: "Favoritos"},
     {key: "recent", label: "Mas jugados"},
 ];
+
+//extraemos el payload del JWT del compañero
+
+function getUserIdFromToken (token) {
+    try {
+        return JSON.parse(atob(token.split(".")[1])).sub;
+    }   catch{
+        return null;
+    }
+}
 
 //Endpoints para sacar los juegos del usuario
 
