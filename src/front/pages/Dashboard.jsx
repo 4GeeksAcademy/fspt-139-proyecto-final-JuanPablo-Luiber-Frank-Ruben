f0f4da51a6e225af25bff1781760e09d2dde5153
frankybanezmijichich <frank.ybanez@gmail.com>
2026-09-08 14:02:10 +0000

    Dasboard

---
+++ src/front/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+export const Dashboard = () => {
+    return (
+        <div className="container mt-5">
+            <h1>Dashboard</h1>
+            <p>Esto es un placeholder, después reempalazar</p>
+        </div>
+
+
+    );
+};
