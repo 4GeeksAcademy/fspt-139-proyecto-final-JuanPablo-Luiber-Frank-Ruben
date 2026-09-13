ecf89f28895b9e8d2b54fa338c0b022f05c378df
rubensasa <rebensasa@gmail.com>
2026-09-13 14:59:58 +0000

    empezado el navbar de la pagina

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,8 +1,56 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 export const Navbar = () => {
 
+	const navigate = useNavigate();
+	const location = useLocation();
+	const [Token, setToken] = useState(localStorage.getItem)
+
+	useEffect(() => {
+		setToken(localStorage.getItem("token"));
+	}, [location]);
+
+	const handleLogout = () => {
+		localStorage.removeItem("token");
+		setToken(null);
+		navigate("/login");
+	};
+
+	const isActive = (path) => (location.pathname === path ? "active" : "");
+
 	return (
+
+		<nav className = "navbar navbar-expand-lg navbar-dar bg-dark">
+			<div className = "container">
+				
+				<link to = {token ? "/profile" : "/login"} className="navbar-brand d-flex align-items-center gap-2">
+					<i className="fa-solid fa-gamepad"></i>
+					<span>Steam<stron className ="text-primary">View</stron></span>
+				</link>
+
+				<button	className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+					aria-controls="navbarNav" aria-expanded="false" aria-label="Abrir menú">
+					
+					<span className="navbar-toggler-icon"></span>
+
+				</button>
+
+				<div className="collapse navbar-collapse" id="navbarNav">
+
+					{token && (
+						
+					)
+					
+					}
+
+				</div>
+			</div>
+		</nav>
+
+	)
+
+/* 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
@@ -15,5 +63,5 @@ export const Navbar = () => {
 				</div>
 			</div>
 		</nav>
-	);
+	); */
 };
